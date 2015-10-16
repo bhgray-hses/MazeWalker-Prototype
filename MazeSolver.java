6d007d734ae84b827e3e41d645f4c6b0e05b4d99
bhgray-hses <bhgray@philasd.org>
2015-10-16 14:19:21 -0400

    initial commit of MazeSolver interface
    fully implemented

---
+++ MazeSolver.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of interface MazeSolver here.
+ * 
+ * @author Brent Gray
+ * @version 2015-10-16-1414
+ */
+public interface MazeSolver
+{
+    /**
+     * commands a Robot to solve a maze
+     * 
+     */
+    void solveMaze();
+}
